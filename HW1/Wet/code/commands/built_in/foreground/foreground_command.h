f71b72447530c1a2575b79a10bfabfd5667f058e
eilon-code <eilon.halevy12@gmail.com>
2024-12-10 04:56:07 +0200

    idea of fg for now

--- HW1/Wet/code/commands/built_in/foreground/foreground_command.h
+++ HW1/Wet/code/commands/built_in/foreground/foreground_command.h
@@ -1,10 +1,33 @@
-class ForegroundCommand : public BuiltInCommand {
-    // TODO: Add your data members
-public:
-    ForegroundCommand(const char *cmd_line, JobsList *jobs);
+#ifndef FOREGROUND_COMMAND_H_
+#define FOREGROUND_COMMAND_H_
 
-    virtual ~ForegroundCommand() {
+#include <unistd.h> // getcwd
+#include <iostream> // std::cout and std::cerr
+#include "built_in/built_in_command.h"
+#include "code/smash/Smash.h"
+#include "code/joblist/joblist.h"
+
+class ForegroundCommand : public BuiltInCommand {
+public:
+    ForegroundCommand(const char *cmd_line) : BuiltInCommand(cmd_line) {
+        m_cmd_line = cmd_line;
     }
 
-    void execute() override;
+    ~ForegroundCommand() = default;
+
+    void execute() override {
+        SmallShell &smash = SmallShell::getInstance();
+        JobsList& jobsList = smash.getJobsList();
+        // find id or null id
+        bool isIdInput = false;
+        // jobsList.getJobById();
+        if (isIdInput) {
+
+        }
+        
+    }
+private:
+    std::string m_cmd_line;
 };
+
+#endif // FOREGROUND_COMMAND_H_
