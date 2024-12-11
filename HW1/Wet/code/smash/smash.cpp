3011d3e6857efc11f74f9d7d91f59dbfa0da8ee4
eilon-code <eilon.halevy12@gmail.com>
2024-12-12 00:09:06 +0200

    added I/O redirection

--- HW1/Wet/code/smash/smash.cpp
+++ HW1/Wet/code/smash/smash.cpp
@@ -18,13 +18,11 @@
 #include "../built_in/special/listdir/listdir_command.h"
 #include "../built_in/special/whoami/who_am_i_command.h"
 
-using namespace std;
-
 /**
 * Creates and returns a pointer to Command class which matches the given command line (cmd_line)
 */
 Command* SmallShell::createCommand(const char *cmd_line) {
-	string cmd_s = _trim(string(cmd_line));
+	std::string cmd_s = _trim(string(cmd_line));
 	// apply alias here
 
 	char** args;
@@ -39,7 +37,7 @@ Command* SmallShell::createCommand(const char *cmd_line) {
 }
 
 BuiltInCommand* _createBuiltInCommand(char** args, int words) {
-	string firstWord = strcpy(args[0]);
+	std::string firstWord = args[0];
 	_removeBackgroundSign(firstWord);
 
 	if (firstWord.compare("chprompt") == 0) {
@@ -77,7 +75,7 @@ BuiltInCommand* _createBuiltInCommand(char** args, int words) {
 }
 
 ExternalCommand* _createExternalCommand(char** args, int words) {
-	string firstWord = args[0];
+	std::string firstWord = args[0];
 	bool isBackground = _isBackgroundComamnd();
 	if (words == 1) {
 
@@ -89,10 +87,25 @@ ExternalCommand* _createExternalCommand(char** args, int words) {
 	return nullptr;
 }
 
+void _redirectIO(Command* cmd, const char *cmd_line) {
+	// check whether > or >> is in cmd_line and apply cmd->setRedirectionFile accordingly.
+	std::string commandStr(cmd_line);
+    size_t redirectionPos = commandStr.find_first_of('>');
+    if (redirectionPos != std::string::npos) {
+        bool append = (redirectionPos + 1 < commandStr.size() && commandStr[redirectionPos + 1] == '>');
+        size_t fileStart = redirectionPos + (append ? 2 : 1);
+		std::string secHalf = commandStr.substr(fileStart, commandStr.size()-fileStart);
+        std::string file = _trim(secHalf);
+
+        cmd->setRedirectionFile(file, append);
+    }
+}
+
 void SmallShell::executeCommand(const char *cmd_line) {
     Command* cmd = createCommand(cmd_line);
 	m_jobsList.removeFinishedJobs(); // removing finished jobs before executing cmd
 	if (cmd != nullptr) {
+		_redirectIO(cmd, cmd_line);
         cmd->execute();
         delete cmd; // Prevent memory leaks
     }
