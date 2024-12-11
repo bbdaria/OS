f891f6ef0a882c0f25d2e91309f86798ff3bb1aa
eilon-code <eilon.halevy12@gmail.com>
2024-12-11 20:11:28 +0200

    start of I/O redirection

--- HW1/Wet/code/commands/Commands.h
+++ HW1/Wet/code/commands/Commands.h
@@ -5,16 +5,34 @@
 #define COMMAND_MAX_ARGS (20)
 
 class Command {
-// TODO: Add your data members
 public:
-    Command()=default;
+    Command() : m_isRedirection(false) {}
     ~Command()=default;
 
     virtual void execute() = 0;
 
     //virtual void prepare();
     //virtual void cleanup();
-    // TODO: Add your extra methods if needed
+
+    void setRedirectionFile(const std::string& file, bool overideContent) {
+        // init file according to m_redirectionFile and m_overrideRedirection
+        m_redirectionFile = file;
+        m_overrideRedirection = overideContent;
+    }
+
+    void printOut(const std::string& str) {
+        if (m_isRedirection) {
+            // do print to file
+        }
+        else {
+            std::cout << str;
+        }
+    }
+
+private:
+    bool m_isRedirection;
+    std::string m_redirectionFile;
+    bool m_overrideRedirection; // symbols of redirection (> or >>)
 };
 
 #endif //SMASH_COMMAND_H_
