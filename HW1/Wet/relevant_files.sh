e9287e9cd709e098dbb12b4fde3483ac691421a4
eilon-code <eilon.halevy12@gmail.com>
2024-12-15 01:33:10 +0200

    more dynamic submission

--- HW1/Wet/relevant_files.sh
+++ HW1/Wet/relevant_files.sh
@@ -1,51 +1,26 @@
 #!/bin/bash
 # generate_file_list.sh
-# Generates a list of files to zip and saves them to a temporary file.
+# Dynamically generates a list of .cpp and .h files to zip, along with additional files.
 
 RELATIVE_PATH="HW1/Wet/code"
 OUTPUT_FILE="files_to_zip.txt" # This is the output file with the list of files
 
-cd $RELATIVE_PATH
+cd $RELATIVE_PATH || { echo "Error: Directory $RELATIVE_PATH not found."; exit 1; }
 
-# Pre-defined list of files (modify this list as needed)
-FILES=(
-    "commands/Commands.h"
-    
-    "commands/built_in/built_in_command.h"
-    "commands/built_in/alias/alias_command.h"
-    "commands/built_in/alias/unalias_command.h"
-    "commands/built_in/change_dir/change_dir_command.h"
-    "commands/built_in/chprompt/chprompt_command.h"
-    "commands/built_in/foreground/foreground_command.h"
-    "commands/built_in/get_current_dir/get_current_dir_command.h"
-    "commands/built_in/jobs/jobs_command.h"
-    "commands/built_in/kill/kill_command.h"
-    "commands/built_in/quit/quit_command.h"
-    "commands/built_in/show_pid/show_pid_command.h"
-    "commands/built_in/special/listdir/listdir_command.h"
-    "commands/built_in/special/net_info/net_info_command.h"
-    "commands/built_in/special/whoami/who_am_i_command.h"
+# Dynamically find all .cpp and .h files under the code directory
+find . -type f \( -name "*.cpp" -o -name "*.h" \) | sed "s|^./|$RELATIVE_PATH/|" >> "../../../$OUTPUT_FILE"
 
-    "commands/external/external_command.h"
-    "commands/external/complex/complex_command.h"
-    "commands/external/simple/simple_command.h"
-    "commands/external/special/pipe/pipe_command.h"
-
-    "joblist/joblist.h"
-    "signals/signals.cpp"
-    "signals/signals.h"
-    "smash/smash.cpp"
-    "smash/smash.h"
-    "main.cpp"
-    "util.cpp"
+# Include additional files such as Makefile
+ADDITIONAL_FILES=(
     "../Makefile"
-    # "submitters.txt" (part of GLOBAL_FILES)
+    # "../submitters.txt" (will be added in GLOBAL_FILES)
 )
 
-# Write the file paths to the output file
-for file in "${FILES[@]}"; do
-    echo "$RELATIVE_PATH/$file" >> "../../../$OUTPUT_FILE"
+# Append additional files to the output file
+for file in "${ADDITIONAL_FILES[@]}"; do
+    echo "$file" >> "../../../$OUTPUT_FILE"
 done
 
-cd ../../..
+cd -
+
 echo "File list generated in $OUTPUT_FILE"
