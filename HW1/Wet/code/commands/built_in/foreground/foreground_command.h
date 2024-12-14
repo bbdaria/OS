984eea2f9424b517877b3b27c0c1f55f150ae1cc
eilon-code <eilon.halevy12@gmail.com>
2024-12-14 18:41:40 +0200

    fixed bugs (my bad)

--- HW1/Wet/code/commands/built_in/foreground/foreground_command.h
+++ HW1/Wet/code/commands/built_in/foreground/foreground_command.h
@@ -9,19 +9,52 @@
 
 class ForegroundCommand : public BuiltInCommand {
 public:
-    ForegroundCommand(char** args, int words) : m_error(false), m_args(args), m_words(words) {}
+    ForegroundCommand(char** args, int words) : m_error(false) {
+        m_isMax = (words == 1);
+        if (m_isMax) return;
+
+        std::string id = args[1];
+        if (words > 2 || !_isDigitsOnly(id.c_str())) {
+            m_err_msg = "smash error: fg: invalid arguments";
+            m_error = true;
+        }
+        else {
+            m_jobId = std::stoi(id);
+        }
+    }
 
     ~ForegroundCommand() = default;
 
     void execute() override {
-        init();
         if (m_error) {
             std::cerr << m_err_msg << std::endl;
             return;
         }
+
         SmallShell &smash = SmallShell::getInstance();
         JobsList& jobsList = smash.getJobsList();
+
+        if (m_isMax) {
+            m_jobId = jobsList.getMaxId();
+        }
+        if (m_jobId == 0) {
+            m_err_msg = "smash error: fg: jobs list is empty";
+            m_error = true;
+            std::cerr << m_err_msg << std::endl;
+            return;
+        }
+
         JobsList::JobEntry* jobEntry = jobsList.removeJobById(m_jobId);
+
+        if (jobEntry == nullptr) {
+            m_err_msg = "smash error: fg: job-id ";
+            m_err_msg.append(std::to_string(m_jobId));
+            std::string a = " does not exist";
+            m_err_msg.append(a.c_str());
+            m_error = true;
+            std::cerr << m_err_msg << std::endl;
+            return;
+        }
         smash.setForeGround(jobEntry);
         int fgPID = jobEntry->getPID();
         std::string cmd_line = jobEntry->getCmdLine();
@@ -35,36 +68,9 @@ public:
 private:
     bool m_error;
     std::string m_err_msg;
+    
     int m_jobId;
-
-    char** m_args;
-    int m_words;
-
-    void init() {
-        SmallShell &smash = SmallShell::getInstance();
-        JobsList& jobsList = smash.getJobsList();
-        if (m_words == 1) {
-            m_jobId = jobsList.getMaxId();
-        }
-        else if (m_words > 2 || (!_isDigitsOnly(m_args[1]))) {
-            m_err_msg = "smash error: fg: invalid arguments";
-            m_error = true;
-        }
-        else {
-            m_jobId = std::stoi(m_args[1]);
-            if (!jobsList.contains(m_jobId)) {
-                m_err_msg = "smash error: fg: job-id ";
-                m_err_msg.append(m_args[1]);
-                std::string a = " does not exist";
-                m_err_msg.append(a.c_str());
-                m_error = true;
-            }
-        }
-        if (jobsList.empty() && !m_error) {
-            m_err_msg = "smash error: fg: jobs list is empty";
-            m_error = true;
-        }
-    }
+    int m_isMax;
 };
 
 #endif // FOREGROUND_COMMAND_H_
